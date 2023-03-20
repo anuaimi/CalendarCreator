9371750b0b95f9217dde4919719bed1e22abab66
Athir Nuaimi <anuaimi@users.noreply.github.com>
2023-03-20 17:23:15 +0000

    rename project

---
+++ target/debug/deps/calendar_creator-39afacbbc2768a4f.d
@@ -0,0 +1,5 @@
+/home/runner/CalendarCreator/target/debug/deps/calendar_creator-39afacbbc2768a4f.rmeta: src/main.rs
+
+/home/runner/CalendarCreator/target/debug/deps/calendar_creator-39afacbbc2768a4f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/calendar_creator-62095d6901f21948.d
@@ -0,0 +1,5 @@
+/home/runner/CalendarCreator/target/debug/deps/calendar_creator-62095d6901f21948.rmeta: src/main.rs
+
+/home/runner/CalendarCreator/target/debug/deps/calendar_creator-62095d6901f21948.d: src/main.rs
+
+src/main.rs:
