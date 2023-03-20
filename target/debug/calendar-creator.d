72b05ac3e23d7439bfeb455681d8c127ced65a0e
Athir Nuaimi <>
2023-03-20 17:19:59 +0000

    Initial commit

---
+++ target/debug/calendar-creator.d
@@ -0,0 +1,1 @@
+/home/runner/CalendarCreator/target/debug/calendar-creator: /home/runner/CalendarCreator/src/main.rs
---
+++ target/debug/deps/calendar_creator-3b5bb1d7520ec2b9.d
@@ -0,0 +1,5 @@
+/home/runner/CalendarCreator/target/debug/deps/calendar_creator-3b5bb1d7520ec2b9: src/main.rs
+
+/home/runner/CalendarCreator/target/debug/deps/calendar_creator-3b5bb1d7520ec2b9.d: src/main.rs
+
+src/main.rs:
